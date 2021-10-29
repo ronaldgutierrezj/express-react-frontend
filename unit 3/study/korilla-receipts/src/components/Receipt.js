37e6ccd31d44cb7c15f627dfd93678f7670b2316
ronald <ronaldgutierrezj@gmail.com>
2021-10-29 08:23:02 -0500

    msg

--- unit 3/study/korilla-receipts/src/components/Receipt.js
+++ unit 3/study/korilla-receipts/src/components/Receipt.js
@@ -4,14 +4,16 @@ const Receipt = (props)=>{
     const order = receipt.order
     
     return <article className="Receipt">
-        <h1>Order for: {receipt.person}</h1>
-        <h3>Drink: {order.drink}</h3>
-        <h3>Main: {order.main}</h3>
+        <h1>{receipt.person}</h1>
+        <h2>{order.main}</h2>
+        <hr/>
         <h3>Protein: {order.protein}</h3>
         <h3>Rice: {order.rice}</h3>
         <h3>Sauce: {order.sauce}</h3>
+        <h3>Drink: {order.drink}</h3>
         <h2>Total</h2>
         <h3>{order.cost}</h3>
+        <h3>{receipt.paid}</h3>
 
 
     </article>

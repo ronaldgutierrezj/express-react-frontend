26483a1c893436ae2831532974310fc69e49e125
ronald <ronaldgutierrezj@gmail.com>
2021-11-04 15:39:35 -0500

    Updates to fix netlify deploy

---
+++ unit 3/deliverable/express_react_lab/frontend/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "http://localhost:4000/ringo-express-react-lab.herokuapp.com/";
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects URL={URL} />
+        </Route>
+        <Route path="/about">
+          <About URL={URL} />
+        </Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

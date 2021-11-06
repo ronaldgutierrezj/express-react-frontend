1add70f51a26fa26d6989acea9b50a0d33f2bf69
ronald <ronaldgutierrezj@gmail.com>
2021-11-06 13:10:23 -0500

    frontend finished

---
+++ unit 3/express-react/frontend/src/components/Main.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
+function Main(props) {
+  const [people, setPeople] = useState(null);
+
+  const URL = "https://ringo-express-react.herokuapp.com/people/";
+
+
+  const getPeople = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setPeople(data);
+  };
+
+  const createPeople = async (person) => {
+    // make post request to create people
+    await fetch(URL, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    // update list of people
+    getPeople();
+  };
+
+  useEffect(() => getPeople(), []);
+
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <Index people={people} createPeople={createPeople} />
+        </Route>
+        <Route
+          path="/people/:id"
+          render={(rp) => (
+            <Show
+              {...rp}
+            />
+          )}
+        />
+      </Switch>
+    </main>
+  );
+}
+
+export default Main;
